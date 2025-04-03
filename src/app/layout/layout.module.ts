9f99a93cadfafaf93cd9bf071dad530bf6101c1f
Surajit Maity <surajit.maity@realpage.com>
2025-04-03 17:13:45 +0530

    added simple hero header and some css imporvements

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
+import { HeroHeaderComponent } from './hero-header/hero-header.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    HeroHeaderComponent,
   ],
   imports: [
     MatButtonModule,
   ],
   exports: [
     HeaderComponent,
+    HeroHeaderComponent,
   ],
 })
 export class LayoutModule { }
